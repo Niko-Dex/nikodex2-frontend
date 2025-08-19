ec0c8f49f12fa0eeb00d7b94a466e316c6dee027
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-08-19 12:49:17 +0700

    added patpat

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -10,12 +10,20 @@
         abilities = [""]
     } = $props()
 
+    let img: HTMLImageElement
+    let patpatAPI = "https://tt7homa.eu.pythonanywhere.com/petpet.gif?image="
+    function patpat() {
+        img.src = patpatAPI + img.src
+    }
+
 </script>
 
 <div class={expanded ? "transition duration-200 fixed w-screen h-screen top-0 left-0 z-5 bg-black/75 flex justify-center items-center" : "w-full"}>
     <div class="border-4 border-amber-600 p-4 bg-black flex gap-4 {expanded ? "max-w-[1200px] m-8" : "max-w-full lg:max-w-[640px]"} w-full flex-col md:flex-row">
         <div class="img flex flex-col gap-2">
-            <img src={img_link} alt="nikosona of {name} by {author}" class="no-antialias max-w-[256px] h-fit">
+            <button class="max-w-[256px] h-fit hover:cursor-grab" onclick={patpat}>
+                <img src={img_link} alt="nikosona of {name} by {author}" class="no-antialias w-100 h-auto" bind:this={img}>
+            </button>
             <button class="btn" onclick={() => { expanded = !expanded }}>{expanded ? "Close" : "View More"}</button>
         </div>
         <div class="info">
