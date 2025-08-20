9f456cc646bfe2c7eea816ee986aec6ef192710e
CDevv <cdevyt@gmail.com>
2025-08-20 12:53:39 +0300

    Query for a niko using id in url param + add niko sprite and text overflow scroll

--- src/routes/(twm)/twm/[id]/+page.svelte
+++ src/routes/(twm)/twm/[id]/+page.svelte
@@ -4,6 +4,7 @@
     import Entity from "$lib/assets/images/twm_page/en.png"
     import Dialogue from "$lib/assets/images/twm_page/niko-background.png"
     import { onMount } from "svelte";
+    let { data } = $props();
 
     let ballon_w = $state(0)
     let ballon_h = $state(0)
@@ -11,19 +12,22 @@
         name: string
         description: string
         author: string
+        img_link: string
     } = $state({
         name: "Loading..",
         description: "Loading..",
-        author: "Loading.."
+        author: "Loading..",
+        img_link: "/api/image?id=1"
     })
 
     async function getData() {
-        fetch(`/api/data/niko/?id=1`)
+        fetch(`/api/data/niko/?id=${data.id}`)
             .then(res => res.json())
             .then(d => {
                 apiData.name = d["name"]
                 apiData.description = d["full_desc"]
                 apiData.author = d["author"]
+                apiData.img_link = d["full_img_path"]
             })
     }
 
@@ -42,14 +46,18 @@
     </div>
     <div class="z-2 fixed top-[350px] w-screen flex flex-col items-center">
         <img alt="Dialogue Balloon" src={Dialogue} class="absolute top-8 no-antialias w-4xs" 
-        bind:clientWidth={ballon_w}>
+        bind:clientWidth={ballon_w} bind:clientHeight={ballon_h}>
         <div class="absolute z-3">
             <p class="text-left" style="min-width: {ballon_w}px;">{apiData.name}</p>
         </div>
         {#if ballon_w > 0}
-            <div class="absolute z-3 top-10 inline-block" style="min-width: calc({ballon_w}px - 22px);">
-                <text class="text-left">{apiData.description}.</text>
+            <div class="relative z-3 top-10 inline-block overflow-scroll" 
+            style="overflow: auto; max-width: calc({ballon_w}px - {ballon_h}px - 22px); max-height: calc({ballon_h}px - 20px);
+            left: calc((-{ballon_h}px / 2))">
+                <text class="text-leftl overflow-auto">{apiData.description}.</text>
             </div>
+            <img src={apiData.img_link} alt="Nikosona"
+            class="absolute z-3 max-w-[96px] max-h-[96px]" style="max-height: 96px; top: 48px; right: calc(({ballon_w}px / 2) + {ballon_h}px - 22px);">
         {/if}
         <div class="absolute z-5 top-40">
             <p class="text-left" style="min-width: {ballon_w}px;">Created by {apiData.author}</p>
