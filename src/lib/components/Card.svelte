02652d21d74c61aa1c0017a9edc8145c016de0d9
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-08-21 12:47:13 +0700

    fix irregular img in noik card

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -47,7 +47,7 @@
 <div class={expanded ? "transition duration-200 fixed w-screen h-screen top-0 left-0 z-5 bg-black/75 flex justify-center items-center" : "w-full"}>
     <div class="border-4 border-amber-600 p-4 bg-black flex gap-4 {expanded ? "max-w-[1200px] m-8" : "max-w-full lg:max-w-[640px]"} w-full flex-col md:flex-row">
         <div class="img flex flex-col gap-2">
-            <button class="max-w-[256px] h-fit hover:cursor-grab" onpointerdown={patpat}>
+            <button class="max-w-[256px] w-[256px] h-fit hover:cursor-grab" onpointerdown={patpat}>
                 <img src={img_link} alt="nikosona of {name} by {author}" class="no-antialias w-100 h-auto" bind:this={img}>
             </button>
             <button class="btn" onclick={() => { expanded = !expanded }}>{expanded ? "Close" : "View More"}</button>
