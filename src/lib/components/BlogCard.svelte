72f0faf4eb66ef300d2472c6885231560877fdcd
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-09-10 16:08:06 +0700

    small fix to blogcard

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -10,8 +10,8 @@
     } = $props()
 </script>
 
-<div class={expanded ? "transition duration-200 bg-black/75 fixed w-screen h-screen top-0 left-0 z-5 flex justify-center items-center" : "w-full"}>
-    <div class={`border-4 border-amber-600 bg-black flex flex-col p-4 gap-4 ${expanded ? "w-3xl" : ""}`}>
+<div class={expanded ? "transition duration-200 bg-black/75 fixed w-screen h-screen top-0 left-0 z-5 flex justify-center" : "w-full"}>
+    <div class={`border-4 border-amber-600 bg-black flex flex-col p-4 gap-4 ${expanded ? "w-3xl my-auto overflow-auto max-h-screen" : ""}`}>
         <div class="main-info flex flex-col">
             <h2 class="h2-txt-size w-fit">{title}</h2>
             <p class="w-fit px-1 bg-white text-black">by {author}</p>
