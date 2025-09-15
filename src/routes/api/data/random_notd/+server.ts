a2ad14c4406500573bcf40aa59b3ba0b77a3f0bd
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-09-15 15:03:39 +0700

    use time-based pseudo-random number generator to pick a NoTD (prevent NoTD from getting reset when frontend process restarts)

--- src/routes/api/data/random_notd/+server.ts
+++ src/routes/api/data/random_notd/+server.ts
@@ -3,8 +3,10 @@ import { env } from "$env/dynamic/private"
 import { addDays, isAfter, startOfDay } from "date-fns"
 import { format, toZonedTime } from "date-fns-tz"
 import { errSrv } from '../../helper';
+import { createHash } from "crypto"
+import nodeMID from "node-machine-id"
 
-let cachedData: Object | null = null
+let cachedData: Object[] = []
 const getNextDate = (curday: Date) => {
     const nowUtc = toZonedTime(curday, "UTC")
     return startOfDay(addDays(nowUtc, 1))
@@ -13,16 +15,25 @@ let nextDate: Date = new Date(0)
 
 export async function GET({ request, fetch, cookies }) {
     let currDate = toZonedTime(new Date(), "UTC")
+
     try {
         if (isAfter(currDate, nextDate)) {
-            const res = await fetch(`${env.API_SERVER_URL}/nikos/random`)
+            const res = await fetch(`${env.API_SERVER_URL}/nikos`)
             if (!res.ok) {
                 return json({ error: "buh" }, { status: res.status })
             }
             cachedData = await res.json()
             nextDate = getNextDate(currDate)
         }
-        return json(cachedData, {
+
+        const magic = createHash("sha256")
+            .update(await nodeMID.machineId())
+            .update(format(currDate, "ddMMyyyy"))
+            .digest()
+
+        const idx = magic.reduce((t, c) => (t + c) % cachedData.length)
+
+        return json(cachedData[idx], {
             headers: {
                 "Expires": format(nextDate, "EEE, dd MMM yyyy HH:mm:ss 'GMT'", {
                     timeZone: "Etc/GMT"
