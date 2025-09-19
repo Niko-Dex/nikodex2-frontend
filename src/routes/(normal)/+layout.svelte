b984dc06583859c07acd7ff234193c20f2735122
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-09-19 09:21:56 +0700

    added theme color button at navbar. change element to follow same theme color as the theme-color variable

--- src/routes/(normal)/+layout.svelte
+++ src/routes/(normal)/+layout.svelte
@@ -7,6 +7,10 @@
     import { onMount } from 'svelte';
     import { afterNavigate } from '$app/navigation';
 
+	import ThemeBtn from "$lib/assets/images/components/theme.png"
+	import CloseBtn from "$lib/assets/images/components/close.png"
+	import Theme from "$lib/assets/data/theme.json"
+
 	let { children } = $props();
 	let errorContactingAPI = $state(false)
 
@@ -27,6 +31,8 @@
 		await checkAPI()
 	})
 
+	let themeOpen = $state(false)
+	let navBarOpen = $state(false)
 </script>
 
 <svelte:head>
@@ -61,24 +67,46 @@
 </div>
 {:else}
 <Transition />
-<nav class="bg-black sticky top-0 w-full border-b-4 border-amber-600 px-4 py-2 justify-between items-center flex flex-col gap-2 lg:flex-row z-3">
-	<div class="logo">
+<nav class="bg-black sticky top-0 w-full border-b-4 border-(--theme-color) px-4 py-2 justify-between items-center flex flex-col gap-2 lg:flex-row z-3">
+	<div class="logo flex items-center w-full lg:w-max justify-between">
 		<a href="/">
 			<img src={logo} alt="Logo" style="--shadow: 2px 2px red" class="max-h-12 hover:drop-shadow-(--shadow) transition duration-100">
 		</a>
+		<button aria-label="Menu" class="hover:cursor-pointer" onclick={() => navBarOpen = !navBarOpen}>
+			<svg class="lg:hidden" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+		</button>
 	</div>
 
-	<div class="link flex gap-4 flex-shrink w-full overflow-auto lg:w-auto">
-		<Link custom_class="flex-1 lg:flex-none" href="/noik">The Niko List</Link>
+	<div class="link gap-2 md:gap-4 px-4 sm:px-0 flex-shrink w-full overflow-auto lg:w-auto flex-col sm:flex-row flex-wrap lg:flex-nowrap justify-end {navBarOpen ? 'flex' : 'hidden lg:flex'}">
+		<Link custom_class="flex-1 lg:flex-none border-(--theme-color)" href="/noik">The Niko List</Link>
 		<Link custom_class="flex-1 lg:flex-none" href="/submit">Submit a Niko</Link>
 		<Link custom_class="flex-1 lg:flex-none" href="/blog">Blog</Link>
+		<button class="md:p-[4px] no-antialias btn flex items-center gap-4 justify-between" onclick={() => themeOpen = !themeOpen} aria-label="Theme changer">
+			<span class="sm:hidden">Change theme</span>
+			<div class="w-[32px] h-[32px] no-antialias" style="mask: url({themeOpen ? CloseBtn : ThemeBtn}) no-repeat center; -webkit-mask: url({themeOpen ? CloseBtn : ThemeBtn}) no-repeat center; -webkit-mask-size: contain; mask-size: contain; background-color: var(--theme-color)"></div>
+		</button>
 	</div>
 </nav>
+
+<div class="sticky top-68 sm:top-18 z-5 w-full">
+	<div class="absolute top-4 right-0 max-w-[480px] w-full h-[360px] bg-black border-(--theme-color) border-4 {themeOpen ? '' : 'hidden'} flex flex-wrap overflow-auto justify-center">
+		{#each Theme.colors as color}
+			<button class="btn p-2 m-4 min-[360px]:max-w-[120px] w-full flex justify-center items-center flex-col" onclick={() => { document.body.style.setProperty("--theme-color", color.color); localStorage.setItem("color", color.color) }}>
+				<div class="w-16 h-16 no-antialias" style="mask: url({ThemeBtn}) no-repeat center; -webkit-mask: url({ThemeBtn}) no-repeat center; -webkit-mask-size: contain; mask-size: contain; background-color: {color.color}"></div>
+				<p>{color.name}</p>
+			</button>
+		{/each}
+		<div class="border-4 border-(--theme-color) p-2 m-4 sm:max-w-[120px] w-full flex justify-center items-center flex-col">
+			<input type="color" aria-label="Custom color" oninput={ev => { let value = (ev.target as HTMLInputElement).value; document.body.style.setProperty("--theme-color", value); localStorage.setItem("color", value) }}>
+			<p><em>Custom</em></p>
+		</div>
+	</div>
+</div>
 
 {@render children?.()}
 
 <footer class="bg-white text-black p-4">
-	<p>The NikoDex v2 is a project created by the contributors of <a class="underline" href="https://github.com/Niko-Dex">Niko-Dex</a>, which are fans of OneShot, and are part of the OneShot Discord Server.</p>
+	<p>The NikoDex v2 is a project created by the contributors of <a href="https://github.com/Niko-Dex">Niko-Dex</a>, which are fans of OneShot, and are part of the OneShot Discord Server.</p>
 
 	<details>
 		<summary>Acknowledgements (for assets not created by the Niko-Dex contributors)</summary>
@@ -86,7 +114,7 @@
 			<ul class="list-disc">
 				<li>Background for the Home page, the Niko List page, the Blog page, the Submit page, and the Error page, along with the Niko sprites are taken from the game OneShot and OneShot WME. All rights for these media belongs to nightmargin.</li>
 				<li>All Nikosonas belongs to their respective owners and artists.</li>
-				<li>Terminus TTF font used in the website are licensed under the SIL Open Font License, downloaded from <a class="underline" href="https://files.ax86.net/terminus-ttf/">https://files.ax86.net/terminus-ttf/</a></li>
+				<li>Terminus TTF font used in the website are licensed under the SIL Open Font License, downloaded from <a href="https://files.ax86.net/terminus-ttf/">https://files.ax86.net/terminus-ttf/</a></li>
 				<li>Svelte, TailwindCSS, along with a few dependecies, were used in the creation of the front-end.</li>
 			</ul>
 		</div>
