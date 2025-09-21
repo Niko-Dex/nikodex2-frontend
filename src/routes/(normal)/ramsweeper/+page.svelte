d09425dad4a590c927fbff487d18213fe51fc0b7
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-09-21 19:38:33 +0700

    improve experience for mobile user

--- src/routes/(normal)/ramsweeper/+page.svelte
+++ src/routes/(normal)/ramsweeper/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import { onMount, tick } from "svelte";
     import {
         tileMetadata, tileMap,
         nikoMap, nikoMetadata,
@@ -32,6 +32,10 @@
     let pancakesLeft = $state(0)
 
     let achievementShow = $state(false)
+    let mobileActionShow = $state(false)
+    let mobileActionDig = $state() as HTMLButtonElement
+    let mobileActionMark = $state() as HTMLButtonElement
+    let mobileMode = $state(false);
 
     let settings = $state({
         w: 9,
@@ -209,14 +213,20 @@
         resizeCanvas()
         // resizeElement()
 
-        let padding = [ 0, 0 ]
+        let padding = [ 64, 64 ]
 
-        const { width, height } = container.getBoundingClientRect()
+        const containerDimension = container.getBoundingClientRect()
+        const canvasDimension = canvas.getBoundingClientRect()
         let [ viewW, viewH ] = [ innerWidth, innerHeight ]
 
+        const [ deltaW, deltaH ] = [
+            containerDimension.width - canvasDimension.width,
+            containerDimension.height - canvasDimension.height
+        ]
+
         let scale = Math.min(
-            floorToFactor(viewW / (width + padding[0]), 5),
-            floorToFactor(viewH / (height + padding[1]), 5),
+            floorToFactor((viewW - deltaW) / canvasDimension.width, 4),
+            floorToFactor((viewH - deltaH) / canvasDimension.height, 4),
         )
 
         settings.scale = scale
@@ -339,7 +349,21 @@
         }
     }
 
-    function checkAndUpdate(mouseClick: number, x: number, y: number) {
+    function awaitMobileAction() {
+        return new Promise<number>((res) => {
+            mobileActionDig.onclick = () => res(0)
+            mobileActionMark.onclick = () => res(2)
+        })
+    }
+
+    async function checkAndUpdate(mouseClick: number, x: number, y: number) {
+        if (mobileMode) {
+            mobileActionShow = true
+            await tick()
+            mouseClick = await awaitMobileAction()
+            mobileActionShow = false
+        }
+
         if (mouseClick == 2) {
             if (gameBoardPancake[x][y]) {
                 pancakesLeft++
@@ -420,7 +444,7 @@
         }
     }
 
-    function clickUp(ev: MouseEvent) {
+    async function clickUp(ev: MouseEvent) {
         if (!gameState.inProgress) return
         btnState("normal")
         const [tileX, tileY] = getTilePos(ev)
@@ -433,7 +457,7 @@
             } catch (e) {}
         }
         if (tileX < 0 || tileY < 0 || tileX >= gameState.size[0] || tileY >= gameState.size[1]) return
-        checkAndUpdate(ev.button, tileX, tileY)
+        await checkAndUpdate(ev.button, tileX, tileY)
         checkWin()
 
         if (!gameState.haveClickFirst) timeAnim = requestAnimationFrame(countTime)
@@ -516,6 +540,10 @@
             setAchievement(Achievement.Baaaaaa)
         }
 
+        //@ts-ignore
+        // http://detectmobilebrowsers.com/
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) mobileMode = true;})(navigator.userAgent||navigator.vendor||window.opera);
+
         ctx = canvas.getContext("2d")
         image = await loadImage(Tiles)
 
@@ -529,13 +557,24 @@
             autoResize()
         })
 
-        addEventListener("mousedown", clickDown)
+        canvas.addEventListener("mousedown", clickDown)
         addEventListener("mouseup", clickUp)
         canvas.addEventListener("contextmenu", (ev) => ev.preventDefault())
 
         ready = true
     })
 </script>
+
+<style>
+    .sprite {
+        width: 64px;
+        height: 64px;
+        background-image: var(--action-image);
+        background-size: calc(32px * 5 * 2) calc(32px * 3 * 2);
+        background-repeat: no-repeat;
+    }
+</style>
+
 <Toaster></Toaster>
 <div class="flex flex-col gap-4 w-min m-4 no-antialias mx-auto max-w-full px-4" bind:this={container}>
     <h1 class="h1-txt-size text-center">Ramsweeper!</h1>
@@ -578,19 +617,20 @@
         <p class="text-sm">you may need to scroll if you device is small --&gt;</p>
         <canvas width="360" height="360" bind:this={canvas} class="mx-auto"></canvas>
     </div>
-    <div class="flex gap-4">
+    <div class="flex gap-4 flex-wrap">
+        <button class="btn grow" onclick={() => { mobileMode = !mobileMode }}>Mobile mode: {mobileMode ? "On" : "Off"}</button>
         <button class="btn grow" onclick={() => { achievementShow = true }}>Achievements</button>
     </div>
 </div>
 
 {#if ready && achievementShow}
 <div class="fixed w-screen h-screen top-0 left-0 z-5 bg-black/75 flex justify-center">
-    <div class="border-4 border-(--theme-color) p-4 bg-black flex gap-4 max-w-[1200px] mx-8 my-auto overflow-auto max-h-screen w-full flex-col">
-        <div class="flex justify-between items-center">
+    <div class="border-4 border-(--theme-color) p-4 bg-black flex gap-4 max-w-[1200px] sm:mx-8 my-auto overflow-auto max-h-screen w-full flex-col">
+        <div class="flex justify-between items-center flex-col sm:flex-row">
             <h1 class="h1-txt-size">Achievements</h1>
-            <div>
-                <button class="btn" onclick={() => { localStorage.removeItem("achievements"); achievementShow = false }}>Reset</button>
-                <button class="btn" onclick={() => { achievementShow = false }}>Close</button>
+            <div class="w-full sm:w-min flex gap-4">
+                <button class="btn grow" onclick={() => { localStorage.removeItem("achievements"); achievementShow = false }}>Reset</button>
+                <button class="btn grow" onclick={() => { achievementShow = false }}>Close</button>
             </div>
         </div>
         <div class="flex flex-col gap-4">
@@ -605,6 +645,22 @@
                     {/if}
                 </div>
             {/each}
+        </div>
+    </div>
+</div>
+{/if}
+
+{#if mobileActionShow && mobileMode}
+<div class="fixed w-screen h-screen top-0 left-0 z-5 bg-black/75 flex justify-center no-antialias">
+    <div class="border-4 border-(--theme-color) p-4 bg-black flex gap-4 max-w-[480px] sm:mx-8 my-auto overflow-auto max-h-screen w-full flex-col">
+        <h1 class="h1-txt-size">Select an action</h1>
+        <div class="flex gap-4 flex-wrap">
+            <button class="btn grow flex justify-center items-center" aria-label="Dig" bind:this={mobileActionDig}>
+                <div class="sprite" style="--action-image: url({Tiles}); background-position: -256px -64px"></div>
+            </button>
+            <button class="btn grow flex justify-center items-center" aria-label="Mark" bind:this={mobileActionMark}>
+                <div class="sprite" style="--action-image: url({Tiles}); background-position: -192px -0px"></div>
+            </button>
         </div>
     </div>
 </div>
