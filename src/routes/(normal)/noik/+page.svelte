19d8ef62ccea04c5b375cf0356237a15863c8e31
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-09-25 19:57:21 +0700

    fix a bug where data get "desynced" when changing page too fast

--- src/routes/(normal)/noik/+page.svelte
+++ src/routes/(normal)/noik/+page.svelte
@@ -2,6 +2,7 @@
     import Background from "$lib/assets/images/page/noik/bg1.png"
     import Card from "$lib/components/Card.svelte"
     import CardContainer from "$lib/components/CardContainer.svelte"
+    import { json } from "@sveltejs/kit";
     import { onMount } from "svelte";
 
     function replace_img(ev: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement; }) {
@@ -14,7 +15,6 @@
     let apiData: Niko[] = $state([])
     let maxPages = $state(1)
     let currentPage = $state(1)
-    let isSearching = $state(false)
     let searchQuery = $state("")
     let orderingOpt = $state("oldest_added")
     let pageSelectDetect = $state() as HTMLDivElement
@@ -37,65 +37,69 @@
     async function getData() {
         if (currentPage < 1 || currentPage > maxPages) return
         dataLoaded = false
+        dataErr = false
         apiData = []
-        fetch(`/api/data/page?page=${currentPage}&sort_by=${orderingOpt}`)
-            .then(d => d.json())
-            .then(data => {
-                for (let d of data) {
-                    apiData.push({
-                        name: d["name"],
-                        author: d["author"],
-                        description: d["full_desc"],
-                        short_desc: d["description"],
-                        abilities: d["abilities"].map((v: { name: string }) => v.name),
-                        id: d["id"]
-                    })
-                }
-                dataLoaded = true
-            })
-            .catch(err => {
-                console.log(err)
-                dataErr = true
-            })
+        try {
+            const res = await fetch(`/api/data/page?page=${currentPage}&sort_by=${orderingOpt}`)
+            const data = await res.json()
+
+            for (let d of data) {
+                apiData.push({
+                    name: d["name"],
+                    author: d["author"],
+                    description: d["full_desc"],
+                    short_desc: d["description"],
+                    abilities: d["abilities"].map((v: { name: string }) => v.name),
+                    id: d["id"]
+                })
+            }
+            dataLoaded = true
+        } catch (err) {
+            console.log(err)
+            dataErr = true
+        }
     }
 
     async function getMaxPages() {
-        fetch(`/api/data/count`)
-            .then(d => d.text())
-            .then(data => {
-                maxPages = Math.ceil((Number.parseFloat(data)) / 14.0);
-            })
+        try {
+            dataErr = false
+            const res = await fetch(`/api/data/count`)
+            const data = await res.text()
+            maxPages = Math.ceil((Number.parseFloat(data)) / 14.0);
+        } catch (err) {
+            console.log(err)
+            dataErr = true
+        }
     }
 
     async function getSearchData() {
         if (searchQuery.length === 0) return
         apiData = []
-        isSearching = true
         currentPage = 1
         dataLoaded = false
-        fetch(`/api/data/name?name=${searchQuery}`)
-            .then(d => d.json())
-            .then(data => {
-                for (let d of data) {
-                    apiData.push({
-                        name: d["name"],
-                        author: d["author"],
-                        description: d["full_desc"],
-                        short_desc: d["description"],
-                        abilities: d["abilities"].map((v: { name: string }) => v.name),
-                        id: d["id"]
-                    })
-                }
-                dataLoaded = true
-            })
-            .catch(err => {
-                console.log(err)
-                dataErr = true
-            })
+        dataErr = false
+        try {
+            const res = await fetch(`/api/data/name?name=${searchQuery}`)
+            const data = await res.json()
+
+            for (let d of data) {
+                apiData.push({
+                    name: d["name"],
+                    author: d["author"],
+                    description: d["full_desc"],
+                    short_desc: d["description"],
+                    abilities: d["abilities"].map((v: { name: string }) => v.name),
+                    id: d["id"]
+                })
+            }
+            dataLoaded = true
+        } catch (err) {
+            console.log(err)
+            dataErr = true
+        }
     }
 
     async function clearSearch() {
-        isSearching = false
         searchQuery = ""
         await getData()
     }
@@ -159,15 +163,15 @@
     </div>
 </section>
 <div bind:this={pageSelectDetect}></div>
-{#if !dataErr && !isSearching}
+{#if !dataErr}
     <div class="bg-gray-700 flex justify-center gap-4 p-2 {pageSelectAtBottom ? "w-full" : "p-4 w-fit mx-auto sticky bottom-4"}">
         <button 
         class="transition duration-100 hover:bg-white hover:text-black hover:cursor-pointer border-2 border-white px-4 py-1 disabled:opacity-50 disabled:pointer-events-none"
-        onclick={async() => await prevPage()} disabled={currentPage <= 1}>Prev</button>
+        onclick={prevPage} disabled={currentPage <= 1 || !dataLoaded}>Prev</button>
         <input class="text-center border-2 border-white min-w-20"
-        type="number" min={1} max={maxPages} bind:value={currentPage} aria-label="Page number" onchange={async () => await getData()}>
+        type="number" min={1} max={maxPages} bind:value={currentPage} aria-label="Page number" onchange={async () => await getData()} disabled={!dataLoaded}>
         <button 
         class="transition duration-100 hover:bg-white hover:text-black hover:cursor-pointer border-2 border-white px-4 py-1 disabled:opacity-50 disabled:pointer-events-none"
-        onclick={async() => await nextPage()} disabled={currentPage >= maxPages}>Next</button>
+        onclick={nextPage} disabled={currentPage >= maxPages || !dataLoaded}>Next</button>
     </div>
 {/if}
