890e44157254dc803e07185eb4f50ee733855adf
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-10-10 10:00:37 +0700

    replaced "log out" text with icons for logging out

--- src/routes/(internal)/admin/(dashboard)/+layout.svelte
+++ src/routes/(internal)/admin/(dashboard)/+layout.svelte
@@ -21,7 +21,9 @@
 			<Link custom_class="flex-1 lg:flex-none" href="/admin">Main</Link>
 			<Link custom_class="flex-1 lg:flex-none" href="/admin/noik">Noik</Link>
 			<Link custom_class="flex-1 lg:flex-none" href="/admin/blog">Blog</Link>
-			<Link custom_class="flex-1 lg:flex-none" href="/admin/logout">Logout</Link>
+			<Link custom_class="flex-0 lg:flex-none" hasMinSize={false} href="/admin/logout">
+				<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
+			</Link>
 		</div>
 	</nav>
 	<div class="p-4">
