318363f794c1553b71fa3668a7114eb01da3ac22
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-11-22 16:28:54 +0700

    Update PendingSubmissionCard.svelte

--- src/lib/components/PendingSubmissionCard.svelte
+++ src/lib/components/PendingSubmissionCard.svelte
@@ -14,13 +14,14 @@
   } = $props();
 </script>
 
-<section class="border-4 border-(--theme-color) p-5 bg-black flex flex-row">
+<section class="border-4 border-(--theme-color) p-5 bg-black flex flex-row gap-8">
   <img src={image} class="w-32 h-32" alt="waiting noik :3" />
   <div class="w-full flex flex-col gap-4">
-    <h2 class="text-center h2-txt-size">Submission: {name}</h2>
-    <p class="ml-10"><em>"{description}"</em></p>
-    <p class="text-center">
-      <em> This submission is waiting for moderator approval. :o</em>
+    <h2 class="h2-txt-size">Submission: <span class="bg-white text-black px-1">{name}</span></h2>
+    <p>Description: <em>"{description}"</em></p>
+    <p>Full description: <em>"{full_desc}"</em></p>
+    <p class="text-right">
+      <em>This submission is waiting for moderator approval. :o</em>
     </p>
   </div>
 </section>
