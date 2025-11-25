79b9724220bdc52a64068a2bd15afaee8cb8f9d7
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-11-25 09:33:19 +0700

    remove unused dependecies

--- src/routes/api/data/random_notd/+server.ts
+++ src/routes/api/data/random_notd/+server.ts
@@ -1,10 +1,5 @@
-import { json } from '@sveltejs/kit';
 import { env } from "$env/dynamic/private"
-import { addDays, isAfter, startOfDay } from "date-fns"
-import { format, toZonedTime } from "date-fns-tz"
 import { errSrv, resWithErrHandling } from '../../helper';
-import { createHash } from "crypto"
-import nodeMID from "node-machine-id"
 
 export async function GET({ request, fetch, cookies }) {
     try {
