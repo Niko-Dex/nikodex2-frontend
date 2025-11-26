b05804805d3a481db8eb8bb9b4bbfcfd4054da26
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-11-26 18:27:01 +0700

    make dropdownmenu more versatile

--- src/lib/components/DropdownMenu.svelte
+++ src/lib/components/DropdownMenu.svelte
@@ -1,47 +1,47 @@
 <script lang="ts">
-    import { afterNavigate, onNavigate } from "$app/navigation";
-
-    let {
-        menuData,
-        title,
-    }: {
-        menuData: {
-            linkName: string;
-            linkGoto: string;
-        }[];
-        title: string;
-    } = $props();
+    //? i'll say, this implementation of dropdown is actually brilliant.
+    import { onNavigate } from "$app/navigation";
 
     let isShown = $state(false);
+
+    let {
+        btn_img = "",
+        title,
+        width,
+        height = -1,
+    }: {
+        btn_img?: string;
+        title: string;
+        width: number;
+        height?: number;
+    } = $props();
 
     onNavigate(() => {
         isShown = false;
     });
 </script>
 
-<section class="flex flex-row-reverse">
+<div class="flex flex-row-reverse relative">
     <button
         onclick={() => (isShown = true)}
-        class="btn w-full flex flex-row gap-2 items-center"
+        class="btn w-full flex flex-row gap-2 items-center sm:p-1"
     >
-        <slot />
+        {#if btn_img != ""}
+            <img src={btn_img} width="32" height="32" alt={title} />
+            <span class="sm:hidden">{title}</span>
+        {:else}
+            <span>{title}</span>
+        {/if}
     </button>
     <div
         class="absolute {isShown
             ? 'scale-100'
-            : 'scale-0'} transition-all duration-200 origin-top-right grid gap-2 w-80 h-fit bg-black p-3 border-4 border-(--theme-color) z-50"
+            : 'scale-0'} transition-all duration-200 origin-top-right grid gap-2 h-fit bg-black p-3 border-4 border-(--theme-color) z-50 max-w-full sm:max-w-none"
+        style="width: {width}px; {height > 0 ? `height: ${height}px` : ''}"
     >
         <button class="btn w-fit float-left" onclick={() => (isShown = false)}
             >Close</button
         >
-        <h1 class="h2-txt-size text-white items-center">{title}</h1>
-        {#each menuData as menuPiece}
-            <a
-                class="btn text-white w-full h-full items-center flex flex-row"
-                href={menuPiece.linkGoto}
-            >
-                <p class="text-center w-full">{menuPiece.linkName}</p>
-            </a>
-        {/each}
+        <slot />
     </div>
-</section>
+</div>
