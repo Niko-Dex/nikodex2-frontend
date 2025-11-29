6fbc6c2e4c6f9180f6e949f2d95f2d37ecbce291
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-11-29 10:37:28 +0700

    code linting + small change for disabled elements

--- src/lib/components/FileUpload.svelte
+++ src/lib/components/FileUpload.svelte
@@ -1,31 +1,76 @@
 <script lang="ts">
-
     interface Props {
-        elm?: HTMLInputElement,
-        accept?: string,
-        disabled?: boolean,
-        name?: string,
-        required?: boolean
+        elm?: HTMLInputElement;
+        accept?: string;
+        disabled?: boolean;
+        name?: string;
+        required?: boolean;
     }
 
-    let { elm = $bindable(), accept, disabled, name, required }: Props = $props()
+    let {
+        elm = $bindable(),
+        accept,
+        disabled,
+        name,
+        required,
+    }: Props = $props();
     let files: FileList | null | undefined = $state();
 </script>
+
 <div class="flex gap-2 fill-(--theme-color)">
-    <label class="transition duration-200 border-4 border-(--theme-color) p-2 bg-black block w-full min-w-[300px] hover:bg-(--theme-color) hover:text-black hover:fill-black {disabled ? "pointer-events-none cursor-default" : "hover:cursor-pointer"}">
-        <input type="file" bind:this={elm} class="hidden" accept={accept} disabled={disabled} name={name} bind:files={files} required={required}>
+    <label
+        class="transition duration-200 border-4 border-(--theme-color) p-2 bg-black block w-full min-w-[300px] hover:bg-(--theme-color) hover:text-black hover:fill-black {disabled
+            ? 'pointer-events-none cursor-default brightness-70 disabled:bg-gray-900'
+            : 'hover:cursor-pointer'}"
+    >
+        <input
+            type="file"
+            bind:this={elm}
+            class="hidden"
+            {accept}
+            {disabled}
+            {name}
+            bind:files
+            {required}
+        />
         <div class="flex items-center gap-2 break-all">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                ><path
+                    d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"
+                /></svg
+            >
             {#if files && files.length > 0}
-            File selected: {files[0].name}
+                File selected: {files[0].name}
             {:else if disabled}
-            No uploading! :c
+                No uploading! :c
             {:else}
-            Upload a file pls :3
+                Upload a file pls :3
             {/if}
         </div>
     </label>
-    <button class="hover:cursor-pointer btn p-2 hover:bg-(--theme-color) hover:fill-black" disabled={disabled} onclick={() => { if (elm) {elm.value = ""; files = null} }} aria-label="Clear">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+    <button
+        class="hover:cursor-pointer btn p-2 hover:bg-(--theme-color) hover:fill-black"
+        {disabled}
+        onclick={() => {
+            if (elm) {
+                elm.value = "";
+                files = null;
+            }
+        }}
+        aria-label="Clear"
+    >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            ><path
+                d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+            /></svg
+        >
     </button>
 </div>
