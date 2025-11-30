cf9a8a3312b005fd58d7fd3996a30daa6fa3d006
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-11-30 21:51:53 +0700

    code linting

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,48 +1,48 @@
-import type { Handle } from "@sveltejs/kit"
-import chalk, { type ChalkInstance } from "chalk"
+import type { Handle } from "@sveltejs/kit";
+import chalk, { type ChalkInstance } from "chalk";
 
 export const handle: Handle = async ({ event, resolve }) => {
-    const method = event.request.method;
-	const url = event.url.pathname + event.url.search;
+  const method = event.request.method;
+  const url = event.url.pathname + event.url.search;
 
-    // Process request & capture response
-    const start = Date.now()
-    const response = await resolve(event);
-    const duration = Date.now() - start
+  // Process request & capture response
+  const start = Date.now();
+  const response = await resolve(event);
+  const duration = Date.now() - start;
 
-    const status = response.status
-    let statusColor: ChalkInstance = chalk.white
+  const status = response.status;
+  let statusColor: ChalkInstance = chalk.white;
 
-    if (status >= 500) statusColor = chalk.red;
-	else if (status >= 400) statusColor = chalk.yellow;
-	else if (status >= 300) statusColor = chalk.cyan;
-	else if (status >= 200) statusColor = chalk.green;
+  if (status >= 500) statusColor = chalk.red;
+  else if (status >= 400) statusColor = chalk.yellow;
+  else if (status >= 300) statusColor = chalk.cyan;
+  else if (status >= 200) statusColor = chalk.green;
 
-    let durationColor: ChalkInstance = chalk.greenBright
-    if (duration >= 1000) durationColor = chalk.yellowBright
+  let durationColor: ChalkInstance = chalk.greenBright;
+  if (duration >= 1000) durationColor = chalk.yellowBright;
 
-    let methodColor: ChalkInstance = chalk.cyan
-    switch (method) {
-        case "GET":
-            methodColor = chalk.cyan
-            break
-        case "POST":
-            methodColor = chalk.green
-            break
-        case "PUT":
-            methodColor = chalk.yellow
-            break
-        case "PATCH":
-            methodColor = chalk.magenta
-            break
-        case "DELETE":
-            methodColor = chalk.red
-            break
-    }
+  let methodColor: ChalkInstance = chalk.cyan;
+  switch (method) {
+    case "GET":
+      methodColor = chalk.cyan;
+      break;
+    case "POST":
+      methodColor = chalk.green;
+      break;
+    case "PUT":
+      methodColor = chalk.yellow;
+      break;
+    case "PATCH":
+      methodColor = chalk.magenta;
+      break;
+    case "DELETE":
+      methodColor = chalk.red;
+      break;
+  }
 
-	console.log(
-		`[${new Date().toISOString()} | ${durationColor(duration.toString().padStart(3, ' ') + "ms")}] (${statusColor(response.status)}) ${methodColor(method)} ${chalk.blueBright(url)}`
-	);
+  console.log(
+    `[${new Date().toISOString()} | ${durationColor(duration.toString().padStart(3, " ") + "ms")}] (${statusColor(response.status)}) ${methodColor(method)} ${chalk.blueBright(url)}`,
+  );
 
-    return response
-}
+  return response;
+};
