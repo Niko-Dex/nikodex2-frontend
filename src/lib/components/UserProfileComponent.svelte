f2f3209e7e124c2aa82f26d731a2fbf25bc120f7
D. Quan <60545346+sudoker0@users.noreply.github.com>
2026-01-01 13:19:34 +0700

    redesign of user profile comp.

--- src/lib/components/UserProfileComponent.svelte
+++ src/lib/components/UserProfileComponent.svelte
@@ -1,46 +1,37 @@
 <script lang="ts">
-  import UnmigratedWarningIcon from "$lib/assets/images/page/account/unmigrated_warning_icon.png";
-  let { username, id }: { username: string; id: number } = $props();
+    import UnmigratedWarningIcon from "$lib/assets/images/page/account/unmigrated_warning_icon.png";
+    let { username, id }: { username: string; id: number } = $props();
 </script>
 
 {#if username}
-  <div class="flex flex-row items-center gap-4">
-    By:
-    <a
-      class="w-fit px-1 no-underline h2-txt-size h-fit mt-auto flex items-center gap-4 transition group hover:bg-white p-2 border-4 border-(--theme-color)"
-      href="/account/{username}"
-    >
-      {#if id}
-        <img
-          src="/api/data/user/pfp?id={id}"
-          class="w-16 h-16 group-hover:invert transition"
-          alt="Profile picture of {username}"
-        />
-      {/if}
+    <div class="flex flex-row items-center gap-4">
+        <a
+            class="w-fit no-underline px-1 h-fit mt-auto flex items-center gap-4 transition group"
+            href="/account/{username}"
+        >
+            {#if id >= 0}
+                <img
+                    src="/api/data/user/pfp?id={id}"
+                    class="w-16 h-16"
+                    alt="Profile picture of {username}"
+                />
+            {/if}
+            <div class="flex flex-col">
+                <span class="underline">{username}</span>
 
-      {#if id == undefined}
-        <div class="flex flex-col">
-          <h1 class="group-hover:invert transition">
-            {username}
-          </h1>
-          <em
-            class="text-xl bg-red-500 flex gap-4 items-center text-white group-hover:invert transition"
-          >
-            <img
-              src={UnmigratedWarningIcon}
-              class="w-8"
-              alt="Unmigrated Icon Warning"
-            />
-            Unmigrated Account!
-          </em>
-        </div>
-      {:else}
-        <h1 class="group-hover:invert transition">
-          {username}
-        </h1>
-      {/if}
-    </a>
-  </div>
+                {#if id == undefined}
+                    <span class="flex flex-row items-center gap-2">
+                        <img
+                            src={UnmigratedWarningIcon}
+                            alt="a"
+                            class="w-4 h-4"
+                        />
+                        <em>(Unmigrated account)</em>
+                    </span>
+                {/if}
+            </div>
+        </a>
+    </div>
 {:else}
-  <em>Invalid Profile Link</em>
+    <em>Invalid Profile Link</em>
 {/if}
