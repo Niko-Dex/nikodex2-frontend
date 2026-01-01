49ce78e9692ad902bc957597c53c2ff60661f7a4
D. Quan <60545346+sudoker0@users.noreply.github.com>
2026-01-01 13:43:55 +0700

    use userprofilecomponent for consistency

--- src/lib/components/CommentCard.svelte
+++ src/lib/components/CommentCard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import toast from "svelte-french-toast";
+    import UserProfileComponent from "./UserProfileComponent.svelte";
 
     let {
         id,
@@ -41,15 +42,8 @@
     class="bg-black border-4 border-(--theme-color) p-2 flex flex-col gap-4 relative"
 >
     <div class="flex flex-row items-center gap-4 overflow-auto">
-        <img
-            src={`/api/data/user/pfp?id=${author_id}`}
-            class="w-16 h-16"
-            alt={`Profile picture of ${username}`}
-        />
-        <div class="flex flex-col">
-            <a href={`/account/${username}`}>{username}</a>
-            <span class="small-txt-size">{date}</span>
-        </div>
+        <UserProfileComponent id={author_id} {username} subtext={date}
+        ></UserProfileComponent>
         {#if deletable}
             <button class="ml-auto btn" onclick={async () => deleteComment()}
                 >Delete</button
--- src/lib/components/GoBackButton.svelte
+++ src/lib/components/GoBackButton.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
-  import GoBackIcon from "$lib/assets/images/components/GoBackIcon.svelte";
-  import LogoutIcon from "$lib/assets/images/components/LogoutIcon.svelte";
+    import GoBackIcon from "$lib/assets/images/components/GoBackIcon.svelte";
 
-  let { children, _class = "", _force_link_to = "" } = $props();
+    let { children, _class = "", _force_link_to = "" } = $props();
 </script>
 
 {#if _force_link_to == ""}
-  <button
-    class="btn flex flex-row gap-2 items-center {_class}"
-    onclick={() => history.back()}
-  >
-    <GoBackIcon />
-    {@render children()}
-  </button>
+    <button
+        class="btn flex flex-row gap-2 items-center {_class}"
+        onclick={() => history.back()}
+    >
+        <GoBackIcon />
+        {@render children()}
+    </button>
 {:else}
-  <a class="btn {_class} flex flex-row gap-2" href={_force_link_to}>
-    <GoBackIcon />
-    {@render children()}
-  </a>
+    <a class="btn {_class} flex flex-row gap-2" href={_force_link_to}>
+        <GoBackIcon />
+        {@render children()}
+    </a>
 {/if}
