fde2a743e8c7ca4e89084fca630a9decf6fb8946
thefirey33 <mmethefirey@gmail.com>
2026-02-20 23:42:13 +0300

    change posts background to glen background

--- src/routes/(normal)/post/[id]/+page.svelte
+++ src/routes/(normal)/post/[id]/+page.svelte
@@ -1,129 +1,126 @@
 <script lang="ts">
-    import type { PageProps } from "./$types";
-    import Background from "$lib/assets/images/page/user/world_machine.png";
-    let { data }: PageProps = $props();
-    import CategoryComponent from "$lib/components/CategoryComponent.svelte";
-    import { onMount } from "svelte";
-    import type { NikodexComment } from "$lib/types/comment";
-    import toast from "svelte-french-toast";
-    import CommentCard from "$lib/components/CommentCard.svelte";
-    import { api, dateFormatter } from "$lib/helper/helper";
-    import GoBackButton from "$lib/components/GoBackButton.svelte";
-    import UserProfileComponent from "$lib/components/UserProfileComponent.svelte";
+  import type { PageProps } from "./$types";
+  import Background from "$lib/assets/images/page/posts/courtyard.png";
+  let { data }: PageProps = $props();
+  import CategoryComponent from "$lib/components/CategoryComponent.svelte";
+  import { onMount } from "svelte";
+  import type { NikodexComment } from "$lib/types/comment";
+  import toast from "svelte-french-toast";
+  import CommentCard from "$lib/components/CommentCard.svelte";
+  import { api, dateFormatter } from "$lib/helper/helper";
+  import GoBackButton from "$lib/components/GoBackButton.svelte";
+  import UserProfileComponent from "$lib/components/UserProfileComponent.svelte";
 
-    import { page } from "$app/state";
-    const currentUser = $derived(page.data.currentUser);
-    let commentData: NikodexComment[] = $state([]);
-    async function getComments() {
-        try {
-            commentData = await api(`/api/data/comments?post_id=${data.id}`);
-        } catch (e) {
-            toast.error((e as Error).message);
-        }
+  import { page } from "$app/state";
+  const currentUser = $derived(page.data.currentUser);
+  let commentData: NikodexComment[] = $state([]);
+  async function getComments() {
+    try {
+      commentData = await api(`/api/data/comments?post_id=${data.id}`);
+    } catch (e) {
+      toast.error((e as Error).message);
     }
-    let inputValue = $state("");
-    onMount(async () => {
-        await getComments();
-    });
+  }
+  let inputValue = $state("");
+  onMount(async () => {
+    await getComments();
+  });
 </script>
 
 <svelte:head>
-    <title>NikoDex - {data.postData.title}</title>
+  <title>NikoDex - {data.postData.title}</title>
 </svelte:head>
 
 <section class="w-full relative flex justify-center p-4">
-    <div
-        class="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center no-antialias bg-fixed -z-1"
-        style="background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url({Background}); "
-    ></div>
-    <div class="max-w-[640px] w-[640px] flex flex-col gap-4 min-h-screen">
-        <GoBackButton _class="w-fit">Go Back</GoBackButton>
-        <div class="flex flex-col gap-0.5">
-            <h1 class="h1-txt-size">{data.postData.title}</h1>
-        </div>
-        <UserProfileComponent
-            username={data.postData.user.username}
-            id={data.postData.user.id}
-            subtext={dateFormatter(data.postData.post_datetime)}
-        ></UserProfileComponent>
-        <p>{data.postData.content}</p>
-
-        <div class="w-full flex flex-col items-center gap-4">
-            <div class="max-w-full bg-black border-2 border-(--theme-color)">
-                <img
-                    class="non-pixelated"
-                    alt={`Image of ${data.postData.title}`}
-                    src="/api/posts/image?id={data.postData.id}"
-                />
-            </div>
-        </div>
-        <CategoryComponent categoryName="Comments">
-            {#if currentUser}
-                <form
-                    class="w-full flex gap-2"
-                    method="POST"
-                    onsubmit={async (event) => {
-                        event.preventDefault();
-
-                        const form: HTMLFormElement = event.currentTarget;
-                        const formData = new FormData(form);
-                        formData.append("post_id", data.id);
-                        formData.append("username", currentUser.username);
-
-                        const commentPostFetch = fetch(form.action, {
-                            method: form.method,
-                            body: formData,
-                        }).then(async (r) => {
-                            const response = await r.json();
-                            const responseJson = JSON.parse(response.data);
-                            if (response.status != "200") throw responseJson;
-                            await getComments();
-                        });
-
-                        toast.promise(commentPostFetch, {
-                            loading: "Commenting...",
-                            success: "Commented!",
-                            error: (e) => `${e}`,
-                        });
-                    }}
-                >
-                    <input
-                        type="text"
-                        maxlength="300"
-                        class="border-4 w-full p-3"
-                        placeholder="Type your comment here... Max: 300"
-                        name="comment"
-                        bind:value={inputValue}
-                        required
-                    />
-                    <button class="btn">Comment</button>
-                </form>
-            {:else}
-                <em class="my-2"
-                    >You must have an account to make comments :3</em
-                >
-            {/if}
-
-            <section class="flex flex-col gap-4">
-                {#each commentData as comment, idx (idx)}
-                    <CommentCard
-                        deletable={comment.author_id == currentUser?.id}
-                        author_id={comment.author_id}
-                        username={comment.user.username}
-                        content={comment.content}
-                        id={comment.id}
-                        date={dateFormatter(comment.post_date)}
-                        original_poster={data.postData.user.id ==
-                            comment.author_id}
-                        on_comment_delete={async () => {
-                            await getComments();
-                        }}
-                    />
-                {:else}
-                    <p><em>it's quite barrens here...</em></p>
-                {/each}
-            </section>
-        </CategoryComponent>
-        <br />
+  <div
+    class="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center no-antialias bg-fixed -z-1"
+    style="background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url({Background}); "
+  ></div>
+  <div class="max-w-[640px] w-[640px] flex flex-col gap-4 min-h-screen">
+    <GoBackButton _class="w-fit">Go Back</GoBackButton>
+    <div class="flex flex-col gap-0.5">
+      <h1 class="h1-txt-size">{data.postData.title}</h1>
     </div>
+    <UserProfileComponent
+      username={data.postData.user.username}
+      id={data.postData.user.id}
+      subtext={dateFormatter(data.postData.post_datetime)}
+    ></UserProfileComponent>
+    <p>{data.postData.content}</p>
+
+    <div class="w-full flex flex-col items-center gap-4">
+      <div class="max-w-full bg-black border-2 border-(--theme-color)">
+        <img
+          class="non-pixelated"
+          alt={`Image of ${data.postData.title}`}
+          src="/api/posts/image?id={data.postData.id}"
+        />
+      </div>
+    </div>
+    <CategoryComponent categoryName="Comments">
+      {#if currentUser}
+        <form
+          class="w-full flex gap-2"
+          method="POST"
+          onsubmit={async (event) => {
+            event.preventDefault();
+
+            const form: HTMLFormElement = event.currentTarget;
+            const formData = new FormData(form);
+            formData.append("post_id", data.id);
+            formData.append("username", currentUser.username);
+
+            const commentPostFetch = fetch(form.action, {
+              method: form.method,
+              body: formData,
+            }).then(async (r) => {
+              const response = await r.json();
+              const responseJson = JSON.parse(response.data);
+              if (response.status != "200") throw responseJson;
+              await getComments();
+            });
+
+            toast.promise(commentPostFetch, {
+              loading: "Commenting...",
+              success: "Commented!",
+              error: (e) => `${e}`,
+            });
+          }}
+        >
+          <input
+            type="text"
+            maxlength="300"
+            class="border-4 w-full p-3"
+            placeholder="Type your comment here... Max: 300"
+            name="comment"
+            bind:value={inputValue}
+            required
+          />
+          <button class="btn">Comment</button>
+        </form>
+      {:else}
+        <em class="my-2">You must have an account to make comments :3</em>
+      {/if}
+
+      <section class="flex flex-col gap-4">
+        {#each commentData as comment, idx (idx)}
+          <CommentCard
+            deletable={comment.author_id == currentUser?.id}
+            author_id={comment.author_id}
+            username={comment.user.username}
+            content={comment.content}
+            id={comment.id}
+            date={dateFormatter(comment.post_date)}
+            original_poster={data.postData.user.id == comment.author_id}
+            on_comment_delete={async () => {
+              await getComments();
+            }}
+          />
+        {:else}
+          <p><em>it's quite barrens here...</em></p>
+        {/each}
+      </section>
+    </CategoryComponent>
+    <br />
+  </div>
 </section>
--- src/routes/(normal)/posts/+page.svelte
+++ src/routes/(normal)/posts/+page.svelte
@@ -1,177 +1,166 @@
 <script lang="ts">
-    let { form } = $props();
-    import Background from "$lib/assets/images/page/user/world_machine.png";
-    import { onMount } from "svelte";
-    import { toast } from "svelte-french-toast";
-    import FileUpload from "$lib/components/FileUpload.svelte";
-    import PageChanger from "$lib/components/PageChanger.svelte";
-    import PostCard from "$lib/components/PostCard.svelte";
-    import type { Post } from "$lib/types/post.js";
+  let { form } = $props();
+  import Background from "$lib/assets/images/page/posts/courtyard.png";
+  import { onMount } from "svelte";
+  import { toast } from "svelte-french-toast";
+  import FileUpload from "$lib/components/FileUpload.svelte";
+  import PageChanger from "$lib/components/PageChanger.svelte";
+  import PostCard from "$lib/components/PostCard.svelte";
+  import type { Post } from "$lib/types/post.js";
 
-    import { page } from "$app/state";
-    import { api } from "$lib/helper/helper.js";
-    import MetaTags from "$lib/components/MetaTags.svelte";
-    const currentUser = $derived(page.data.currentUser);
-    let dataErr = $state(false);
-    let maxPages = $state(1);
-    let currentPage = $state(1);
-    let apiData: Post[] = $state([]);
-    let newPostModal = $state(false);
-    let postTitle = $state("");
-    let postContent = $state("");
-    let dataLoaded = $state(false);
+  import { page } from "$app/state";
+  import { api } from "$lib/helper/helper.js";
+  import MetaTags from "$lib/components/MetaTags.svelte";
+  const currentUser = $derived(page.data.currentUser);
+  let dataErr = $state(false);
+  let maxPages = $state(1);
+  let currentPage = $state(1);
+  let apiData: Post[] = $state([]);
+  let newPostModal = $state(false);
+  let postTitle = $state("");
+  let postContent = $state("");
+  let dataLoaded = $state(false);
 
-    if (form?.error) {
-        toast.error(form.msg);
+  if (form?.error) {
+    toast.error(form.msg);
+  }
+
+  function openModal() {
+    postTitle = "";
+    postContent = "";
+    newPostModal = true;
+  }
+
+  function closeModal() {
+    newPostModal = false;
+  }
+
+  async function getData() {
+    if (currentPage < 1 || (maxPages > 0 && currentPage > maxPages)) return;
+
+    dataLoaded = false;
+    dataErr = false;
+
+    try {
+      apiData = await api(`/api/posts/page?page=${currentPage}&count=10`);
+      dataLoaded = true;
+    } catch (err) {
+      toast.error((err as Error).message);
+      dataErr = true;
     }
+  }
 
-    function openModal() {
-        postTitle = "";
-        postContent = "";
-        newPostModal = true;
+  async function getMaxPages() {
+    dataErr = false;
+    try {
+      const res = await fetch(`/api/posts/count`);
+      if (!res.ok) throw new Error("Failed to fetch count");
+
+      const text = await res.text();
+      maxPages = Math.ceil(Number.parseFloat(text) / 10.0);
+    } catch (err) {
+      toast.error((err as Error).message);
+      dataErr = true;
     }
+  }
 
-    function closeModal() {
-        newPostModal = false;
-    }
-
-    async function getData() {
-        if (currentPage < 1 || (maxPages > 0 && currentPage > maxPages)) return;
-
-        dataLoaded = false;
-        dataErr = false;
-
-        try {
-            apiData = await api(`/api/posts/page?page=${currentPage}&count=10`);
-            dataLoaded = true;
-        } catch (err) {
-            toast.error((err as Error).message);
-            dataErr = true;
-        }
-    }
-
-    async function getMaxPages() {
-        dataErr = false;
-        try {
-            const res = await fetch(`/api/posts/count`);
-            if (!res.ok) throw new Error("Failed to fetch count");
-
-            const text = await res.text();
-            maxPages = Math.ceil(Number.parseFloat(text) / 10.0);
-        } catch (err) {
-            toast.error((err as Error).message);
-            dataErr = true;
-        }
-    }
-
-    onMount(async () => {
-        await getMaxPages();
-        await getData();
-    });
+  onMount(async () => {
+    await getMaxPages();
+    await getData();
+  });
 </script>
 
 <MetaTags
-    description="The posts of the community of the Nikodex!"
-    title="The Nikodex // Posts"
-    image="https://github.com/Niko-Dex/nikodex2-frontend/blob/main/src/lib/assets/images/logo.png?raw=true"
-    type="website"
-    url="https://nikodex.net/posts"
+  description="The posts of the community of the Nikodex!"
+  title="The Nikodex // Posts"
+  image="https://github.com/Niko-Dex/nikodex2-frontend/blob/main/src/lib/assets/images/logo.png?raw=true"
+  type="website"
+  url="https://nikodex.net/posts"
 />
 
 {#if newPostModal}
+  <div
+    class="transition duration-200 bg-black/75 fixed w-screen h-screen top-0 left-0 z-5 flex justify-center p-4"
+  >
     <div
-        class="transition duration-200 bg-black/75 fixed w-screen h-screen top-0 left-0 z-5 flex justify-center p-4"
+      class="border-4 border-(--theme-color) p-4 bg-black flex gap-4 mx-8 my-auto md:w-fit w-full"
     >
-        <div
-            class="border-4 border-(--theme-color) p-4 bg-black flex gap-4 mx-8 my-auto md:w-fit w-full"
+      <div class="flex flex-col gap-2 w-full">
+        <h1 class="text-3xl">New Post</h1>
+        <form
+          class="md:min-w-3xl w-full"
+          method="POST"
+          enctype="multipart/form-data"
         >
-            <div class="flex flex-col gap-2 w-full">
-                <h1 class="text-3xl">New Post</h1>
-                <form
-                    class="md:min-w-3xl w-full"
-                    method="POST"
-                    enctype="multipart/form-data"
-                >
-                    <label>
-                        <p>Name</p>
-                        <input
-                            name="title"
-                            class="w-full"
-                            type="text"
-                            bind:value={postTitle}
-                        />
-                    </label>
-                    <label>
-                        <p>Content</p>
-                        <textarea
-                            name="content"
-                            class="w-full"
-                            bind:value={postContent}
-                        ></textarea>
-                    </label>
-                    <label>
-                        <p>Image</p>
-                        <FileUpload
-                            name="file"
-                            accept="image/*"
-                            required={true}
-                        />
-                    </label>
-                    <br />
-                    <input type="submit" class="btn w-full" value="Submit" />
-                </form>
-                <button class="btn" onclick={() => closeModal()}>
-                    Close
-                </button>
-            </div>
-        </div>
+          <label>
+            <p>Name</p>
+            <input
+              name="title"
+              class="w-full"
+              type="text"
+              bind:value={postTitle}
+            />
+          </label>
+          <label>
+            <p>Content</p>
+            <textarea name="content" class="w-full" bind:value={postContent}
+            ></textarea>
+          </label>
+          <label>
+            <p>Image</p>
+            <FileUpload name="file" accept="image/*" required={true} />
+          </label>
+          <br />
+          <input type="submit" class="btn w-full" value="Submit" />
+        </form>
+        <button class="btn" onclick={() => closeModal()}> Close </button>
+      </div>
     </div>
+  </div>
 {/if}
 
 <section class="w-full relative flex justify-center p-4">
-    <div
-        class="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center no-antialias bg-fixed -z-1"
-        style="background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url({Background}); "
-    ></div>
-    <div class="max-w-[1200px] w-[1200px] flex flex-col gap-4 min-h-screen">
-        <div class="flex flex-row justify-between items-center">
-            <h1 class="h1-txt-size">Community Posts!</h1>
-            {#if currentUser}
-                <button class="btn" onclick={() => openModal()}
-                    >Make a Post!</button
-                >
-            {:else}
-                <p><em>You must be logged in to make posts! :o</em></p>
-            {/if}
-        </div>
-        <p>
-            <em
-                >wanna share something? this is the place for you! just don't
-                shitpost here :c</em
-            >
-        </p>
-        {#if dataLoaded}
-            <div class="grid lg:grid-cols-2 grid-cols-1 gap-2">
-                {#each apiData as post, idx (idx)}
-                    <PostCard
-                        id={post.id}
-                        title={post.title}
-                        username={post.user.username}
-                    />
-                {/each}
-            </div>
-        {:else if dataErr}
-            <p class="text-center">woops! something has gone wrong :c</p>
-        {:else}
-            <p class="text-center">loading...</p>
-        {/if}
+  <div
+    class="absolute top-0 left-0 w-full h-full bg-no-repeat bg-cover bg-center no-antialias bg-fixed -z-1"
+    style="background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url({Background}); "
+  ></div>
+  <div class="max-w-[1200px] w-[1200px] flex flex-col gap-4 min-h-screen">
+    <div class="flex flex-row justify-between items-center">
+      <h1 class="h1-txt-size">Community Posts!</h1>
+      {#if currentUser}
+        <button class="btn" onclick={() => openModal()}>Make a Post!</button>
+      {:else}
+        <p><em>You must be logged in to make posts! :o</em></p>
+      {/if}
     </div>
+    <p>
+      <em
+        >wanna share something? this is the place for you! just don't shitpost
+        here :c</em
+      >
+    </p>
+    {#if dataLoaded}
+      <div class="grid lg:grid-cols-2 grid-cols-1 gap-2">
+        {#each apiData as post, idx (idx)}
+          <PostCard
+            id={post.id}
+            title={post.title}
+            username={post.user.username}
+          />
+        {/each}
+      </div>
+    {:else if dataErr}
+      <p class="text-center">woops! something has gone wrong :c</p>
+    {:else}
+      <p class="text-center">loading...</p>
+    {/if}
+  </div>
 </section>
 <PageChanger
-    {maxPages}
-    onupdate={async (page) => {
-        currentPage = page;
-        await getData();
-    }}
-    disabled={dataErr}
+  {maxPages}
+  onupdate={async (page) => {
+    currentPage = page;
+    await getData();
+  }}
+  disabled={dataErr}
 />
